c931cd994c67202df45769f9d303b5b9e0c4a8a2
39rep <okome3317@gmail.com>
2024-06-08 15:57:47 +0900

    try_vercel_deploy

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@ import app from './app';
 
 const port = 8080;
 
+app.get('/', (req, res) => {
+res.send('Hello, Vercel!');
+});
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
